ddb23833ebe567bf88faf700e07719d3f4d8b9fb
Manish Sharma <manish.sharma0398@gmail.com>
2025-09-29 17:59:02 +0000

    feat: networking related infras

--- networking/resources/variables.tf
+++ networking/resources/variables.tf
@@ -1,30 +1,30 @@
-# variable "environment_slug" {
-#   description = "Environment short name (stg, prd, data)"
-#   type        = string
-# }
+variable "environment_slug" {
+  description = "Environment short name (stg, prd, data)"
+  type        = string
+}
 
-# variable "cidr_block" {
-#   description = "CIDR block for the VPC"
-#   type        = string
-# }
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
 
-# variable "azs" {
-#   description = "List of availability zones"
-#   type        = list(string)
-# }
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
 
-# variable "public_subnets" {
-#   description = "CIDRs for public subnets"
-#   type        = list(string)
-# }
+variable "public_subnets" {
+  description = "CIDRs for public subnets"
+  type        = list(string)
+}
 
-# variable "private_subnets" {
-#   description = "CIDRs for private subnets"
-#   type        = list(string)
-# }
+variable "private_subnets" {
+  description = "CIDRs for private subnets"
+  type        = list(string)
+}
 
-# variable "tags" {
-#   description = "Common tags"
-#   type        = map(string)
-#   default     = {}
-# }
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default     = {}
+}
--- networking/stg/terragrunt.hcl
+++ networking/stg/terragrunt.hcl
@@ -1,26 +1,26 @@
-# include "root" {
-#   path = find_in_parent_folders()
-# }
+include "root" {
+  path = find_in_parent_folders()
+}
 
-# locals {
-#   global_vars = read_terragrunt_config(find_in_parent_folders())
-#   env         = "stg"
-# }
+locals {
+  global_vars = read_terragrunt_config(find_in_parent_folders())
+  env         = "stg"
+}
 
-# terraform {
-#   source = "..//resources"
-# }
+terraform {
+  source = "..//resources"
+}
 
-# inputs = {
-#   environment_slug   = "${local.env}"
-#   cidr_block     = "10.0.0.0/16"
-#   azs            = ["ap-south-2a", "ap-south-2b", "ap-south-2c"]
-#   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+inputs = {
+  environment_slug = "${local.env}"
+  cidr_block       = "10.0.0.0/16"
+  azs              = ["ap-south-2a", "ap-south-2b", "ap-south-2c"]
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
-#   tags = {
-#     Project = "Capstone"
-#     Owner   = "Manish"
-#     Env     = "stg"
-#   }
-# }
+  tags = {
+    Project = "Capstone"
+    Owner   = "Manish"
+    Env     = "stg"
+  }
+}
