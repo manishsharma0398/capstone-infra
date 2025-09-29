a5392973fba7f34b5489b02249fe52d23326f78d
Manish Sharma <manish.sharma0398@gmail.com>
2025-09-29 17:28:22 +0000

    feat: pin tf version

--- networking/resources/main.tf
+++ networking/resources/main.tf
@@ -1,24 +1,24 @@
-# resource "aws_vpc" "main" {
-#   cidr_block           = var.cidr_block
-#   instance_tenancy     = "default"
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+  tags = {
+    Name = "main"
+  }
+}
 
-# resource "aws_subnet" "public" {
-#   count             = length(var.public_subnets)
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = var.public_subnets[count.index]
-#   availability_zone = var.azs[count.index]
-# }
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+}
 
-# resource "aws_subnet" "private" {
-#   count             = length(var.private_subnets)
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = var.private_subnets[count.index]
-#   availability_zone = var.azs[count.index]
-# }
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+}
